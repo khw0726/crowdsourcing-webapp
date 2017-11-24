59983a3a7b40bfc24b2e26a6446101d24eb439a5
soheekim911 <tororok911@gmail.com>
2017-11-24 22:52:16 +0900

    question list

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,32 +1,32 @@
 <template>
-  <div>
-    <h3 id="registration">로그인</h3>
-    <b-form @submit.prevent="onSubmit">
-      <b-form-group id="exampleInputGroup1" label="아이디:" label-for="exampleInput1">
-        <b-form-input id="exampleInput1" :state="isValidName" type="text" v-model="name" required placeholder="아이디를 입력해주세요"></b-form-input>
-      </b-form-group>
-      <b-button type="submit" variant="primary">제출</b-button>
-    </b-form>
-  </div>
+<div>
+  <h3 id="login">로그인</h3>
+  <b-form @submit.prevent="onSubmit">
+    <b-form-group id="exampleInputGroup1" label="아이디:" label-for="exampleInput1">
+      <b-form-input id="exampleInput1" :state="isValidName" type="text" v-model="name" required placeholder="아이디를 입력해주세요"></b-form-input>
+    </b-form-group>
+    <b-button type="submit" variant="primary">로그인</b-button>
+  </b-form>
+</div>
 </template>
 
 <script>
 export default {
   name: 'logIn',
-  data: function () {
+  data: function() {
     return {
       name: '',
       isValidName: null
     }
   },
   methods: {
-    onSubmit: function () {
+    onSubmit: function() {
       this.$root.$firebaseRefs.users.once('value').then(snapshot => {
         const users = snapshot.val()
         console.log(users)
-        for(let user in users) {
+        for (let user in users) {
           console.log(user)
-          if(users[user].name === this.name){
+          if (users[user].name === this.name) {
             this.$store.commit('setAnswererInfo', users[user])
             this.$router.push('questionsList')
           }
@@ -41,5 +41,3 @@ export default {
 <style scoped>
 
 </style>
-
-
--- src/components/PostAnswer/PostAnswer.vue
+++ src/components/PostAnswer/PostAnswer.vue
@@ -1,53 +1,52 @@
 <template>
+<div>
+  <div id="answerimg">
+    <img src="/static/answer.jpg" />
+  </div><br>
+  <div><span>상대방이 화가 났나요?</span></div>
   <div>
-  	<div id="answerimg">
-  		<img src="/static/answer.jpg"/>
-  	</div><br>
-  	<div><span>상대방이 화가 났나요?</span></div>
-  	<div>
     <b-button-group>
-    <b-button>네</b-button>
-    <b-button>아니오</b-button>
+      <b-btn>네</b-btn>
+      <b-btn>아니오</b-btn>
     </b-button-group>
-     </div>
-     <br>
-     <b-form-input v-model="text1"
-                   type="text"
-                   placeholder="다른 의견이 있다면 말씀해주세요"></b-form-input>
-    <div>
-    	 <b-button href="/#/answerComplete">완료</b-button>
-    </div>
   </div>
+  <br>
+  <b-form-input v-model="text1" type="text" placeholder="다른 의견이 있다면 말씀해주세요"></b-form-input>
+  <div>
+    <b-button href="/#/answerComplete">완료</b-button>
+  </div>
+</div>
 </template>
 
 <script>
-  export default {
-  	name: 'postAnswer',
-  	data: function (){
-  	return {
-  		yActive: false,
-  		nActive: false
-  	}
-  	},
-  	methods: {
-      onYActive: function () {
-      this.yActive=true;
-      this.nActive=false;
-      },
-      onNActive: function () {
-      this.nActive=true;
-      this.yActive=false;
-      }
-     }
+export default {
+  name: 'postAnswer',
+  data: function() {
+    return {
+      yActive: false,
+      nActive: false
     }
+  },
+  methods: {
+    onYActive: function() {
+      this.yActive = true;
+      this.nActive = false;
+    },
+    onNActive: function() {
+      this.nActive = true;
+      this.yActive = false;
+    }
+  }
+}
 </script>
 
 <style scoped>
-	#answerimg {
-	width: 50%;
-	margin: auto;
-	}
-  #answerimg img{
-  	max-width:60%;
-  }
+#answerimg {
+  width: 50%;
+  margin: auto;
+}
+
+#answerimg img {
+  max-width: 60%;
+}
 </style>
