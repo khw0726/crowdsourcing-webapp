b6d0b0b57e94a8459dec5e4de398215d06d3e36b
Hyunwoo Kim <khw0726@kaist.ac.kr>
2017-11-26 01:16:58 +0900

    merged again

--- src/components/PostQuestion/WriteQuestion.vue
+++ src/components/PostQuestion/WriteQuestion.vue
@@ -3,11 +3,7 @@
     <h5>무엇을 물어보고 싶은 지 적어주세요.</h5>
     <b-form-input v-model="question" type="text" placeholder="질문을 적어주세요."></b-form-input>
     <br>
-<<<<<<< HEAD
     <b-button @click="onClick">다음</b-button>
-=======
-    <b-button @click="onClick()">다음</b-button>
->>>>>>> 59fdfbf3e33ca1bd2d853a7be125cbb2142d9d4c
   </div>
 </template>
 
