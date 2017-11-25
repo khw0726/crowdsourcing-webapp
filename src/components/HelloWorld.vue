9cb295b7552c35fd1d885abb887303eb9db0ce28
Hyunwoo Kim <khw0726@kaist.ac.kr>
2017-11-25 19:39:13 +0900

    working on push

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,9 +5,10 @@
     <b-form-group id="exampleInputGroup1" label="아이디:" label-for="exampleInput1" description="아이디는 본인 식별 용도로만 사용됩니다.">
       <b-form-input id="exampleInput1" type="text" state="isValidName" v-model="form.name" required placeholder="네 자 이상의 아이디를 입력해주세요"></b-form-input>
     </b-form-group>
-    <b-input-group-button slot="right">
+    <span v-if="isValidName === false">이미 존재하는 아이디입니다.</span>
+    <!-- <b-input-group-button slot="right">
       <b-btn variant="info" @click="checkNameValidity">중복확인</b-btn>
-    </b-input-group-button>
+    </b-input-group-button> -->
     <br>관심 카테고리:
     <b-form-checkbox-group id="checkboxes1" name="interest" v-model="form.selected">
       <b-form-checkbox value="love">연애</b-form-checkbox>
@@ -16,9 +17,12 @@
       <b-form-checkbox value="family">가족</b-form-checkbox>
     </b-form-checkbox-group>
     <br>
-    <b-form-checkbox id="checkbox1" v-model="form.selected" value="accepted" unchecked-value="not_accepted">
+    <b-button @click="registerNotification">
       관심 카테고리의 질문에 대한 알림을 받겠습니다.
-    </b-form-checkbox>
+    </b-button>
+    <!-- <b-form-checkbox id="checkbox1" @click="registerNotification" v-model="form.selected" value="accepted" unchecked-value="not_accepted">
+      관심 카테고리의 질문에 대한 알림을 받겠습니다.
+    </b-form-checkbox> -->
     <br><br>성별:
     <b-form-radio-group id="radio1" name="gender" v-model="form.gender">
       <b-form-radio value="man">남</b-form-radio>
@@ -26,7 +30,7 @@
       <b-form-radio value="etc">기타</b-form-radio>
     </b-form-radio-group>
     <br>연령대:
-    <b-form-radio-group id="radio2" label="checkboxes2" name="age" v-model="form.selected">
+    <b-form-radio-group id="radio2" label="checkboxes2" name="age" v-model="form.age">
       <b-form-radio value="10">10대</b-form-radio>
       <b-form-radio value="20">20대</b-form-radio>
       <b-form-radio value="30">30대</b-form-radio>
@@ -39,6 +43,7 @@
 </div>
 </template>
 <script>
+import fb from '@/fb.js'
 export default {
   data() {
     return {
@@ -49,7 +54,6 @@ export default {
         gender: null,
         pushSubscribed: false,
         selected: []
-
       },
       age: [{
           text: 'Select One',
@@ -61,14 +65,14 @@ export default {
     }
   },
   computed: {
-    isValidName: function() {
-      if (this.form.name.length < 4) {
+    isValidName: function () {
+      if(this.form.name.length < 4){
         return null
       }
       let idx = this.$root.users.findIndex((element) => {
         return element.name === this.form.name
       })
-      return (idx === -1)
+      return (idx === -1) 
     }
   },
   methods: {
@@ -76,6 +80,18 @@ export default {
       this.$root.$firebaseRefs.users.push(this.form)
       this.$store.commit('setAnswererInfo', this.form)
       this.$router.push('seeQuestions')
+    },
+    registerNotification: function () {
+      alert('adsf')
+      fb.messaging.requestPermission()
+      .then(function() {
+        console.log('Notification permission granted.');
+        // TODO(developer): Retrieve an Instance ID token for use with FCM.
+        // ...
+      })
+      .catch(function(err) {
+        console.log('Unable to get permission to notify.', err);
+      });
     }
   }
 }
